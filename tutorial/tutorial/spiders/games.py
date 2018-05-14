0adfc1eee7bda673d8dc0cd24bb1138df3336501
Unknown <fernandosouza@cc.ci.ufpb.br>
2018-05-14 11:12:46 -0300

    Commenting for better reading

--- tutorial/tutorial/spiders/games.py
+++ tutorial/tutorial/spiders/games.py
@@ -2,29 +2,31 @@ import scrapy
 import re
 
 class QuotesSpider(scrapy.Spider):
-    name = "games"
+    name = "games" #Crawler name
     i = 0
     start_urls = [
         'https://www.nuuvem.com/catalog',
     ]
 
     def parse(self, response):
-
+        # gonna locate its price and check if it's a number or a string
+        # if its a number put them together, if not put what is in the String (Free or Unavailable)
         for game in response.css('div.product-card--grid'):
-            price1 = game.css('span span.integer::text').extract_first()
-            price2 = game.css('span span.decimal::text').extract_first()
+            priceInt = game.css('span span.integer::text').extract_first()
+            priceDec = game.css('span span.decimal::text').extract_first()
             priceTxt = game.css('span.product-price--val::text').extract_first()
             
-            if price1 is None:
-                price1 = re.sub(r"[\s]", "", priceTxt)
+            # removing the spaces if it's not a number
+            if priceInt is None:
+                priceInt = re.sub(r"[\s]", "", priceTxt)
             else:
-                price1 = price1+price2
+                priceInt = priceInt+priceDec
             yield {
                 'name': game.css('h3.product-title::text').extract_first(),
-                'price': price1,
+                'price': priceInt,
                 'link': game.css('a.product-card--wrapper::attr(href)').extract_first()
             }
-            
+        # Goes to next item on the list with the links, so the crawler goes to the next page
         self.i +=1
         next_page = response.css('noscript a::attr(href)')[self.i].extract()
         if next_page is not None:
