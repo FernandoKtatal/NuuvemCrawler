4d23e3b21a56ed37880bec1cb5c2c5f95a7960a5
Unknown <fernandosouza@cc.ci.ufpb.br>
2018-05-15 12:27:24 -0300

    Getting the link to the img

--- tutorial/tutorial/spiders/games.py
+++ tutorial/tutorial/spiders/games.py
@@ -11,6 +11,7 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         # gonna locate its price and check if it's a number or a string
         # if its a number put them together, if not put what is in the String (Free or Unavailable)
+        # We use extract_first() so we won't get the item with the [ that comes with it 
         for game in response.css('div.product-card--grid'):
             priceInt = game.css('span span.integer::text').extract_first()
             priceDec = game.css('span span.decimal::text').extract_first()
@@ -24,7 +25,8 @@ class QuotesSpider(scrapy.Spider):
             yield {
                 'name': game.css('h3.product-title::text').extract_first(),
                 'price': priceInt,
-                'link': game.css('a.product-card--wrapper::attr(href)').extract_first()
+                'link': game.css('a.product-card--wrapper::attr(href)').extract_first(),
+                'img': game.css('div.product-img img::attr(src)').extract_first()
             }
         # Goes to next item on the list with the links, so the crawler goes to the next page
         self.i +=1
