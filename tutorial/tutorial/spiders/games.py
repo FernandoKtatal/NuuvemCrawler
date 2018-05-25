367cbdc5f5997f620f16277f91c6717e4a8e590f
Fernando <fernandosouza@cc.ci.ufpb.br>
2018-05-24 22:39:33 -0300

    does not get unavaliables games

--- tutorial/tutorial/spiders/games.py
+++ tutorial/tutorial/spiders/games.py
@@ -7,27 +7,27 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'https://www.nuuvem.com/catalog',
     ]
-
     def parse(self, response):
         # gonna locate its price and check if it's a number or a string
         # if its a number put them together, if not put what is in the String (Free or Unavailable)
         # We use extract_first() so we won't get the item with the [ that comes with it 
-        for game in response.css('div.product-card--grid'):
+        for game in response.css('div.product-card--grid'): 
             priceInt = game.css('span span.integer::text').extract_first()
             priceDec = game.css('span span.decimal::text').extract_first()
             priceTxt = game.css('span.product-price--val::text').extract_first()
-            
             # removing the spaces if it's not a number
-            if priceInt is None:
-                priceInt = re.sub(r"[\s]", "", priceTxt)
-            else:
-                priceInt = priceInt+priceDec
-            yield {
-                'name': game.css('h3.product-title::text').extract_first(),
-                'price': priceInt,
-                'link': game.css('a.product-card--wrapper::attr(href)').extract_first(),
-                'img': game.css('div.product-img img::attr(src)').extract_first()
-            }
+            priceTxt = re.sub(r"[\s]", "", priceTxt)
+            if (priceTxt != "Unavailable"):
+                if priceInt is None:
+                    priceInt = "0,0"
+                else:
+                    priceInt = priceInt+priceDec
+                yield {
+                    'name': game.css('h3.product-title::text').extract_first(),
+                    'price': priceInt,
+                    'link': game.css('a.product-card--wrapper::attr(href)').extract_first(),
+                    'img': game.css('div.product-img img::attr(src)').extract_first()
+                }
         # Goes to next item on the list with the links, so the crawler goes to the next page
         self.i +=1
         next_page = response.css('noscript a::attr(href)')[self.i].extract()
