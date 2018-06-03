0a960460379f3af40ed4e9504f367eb083db20c4
Fernando <fernandosouza@cc.ci.ufpb.br>
2018-06-03 14:11:04 -0300

    steam crawler finally done

--- tutorial/tutorial/spiders/steamgames.py
+++ tutorial/tutorial/spiders/steamgames.py
@@ -3,7 +3,7 @@ import re
 
 class QuotesSpider(scrapy.Spider):
     name = "steamgames" #Crawler name
-    i = 0
+    i = 1
     start_urls = [
         'https://store.steampowered.com/search/?category1=998',
     ]
@@ -11,22 +11,29 @@ class QuotesSpider(scrapy.Spider):
         # gonna locate its price and check if it's a number or a string
         # if its a number put them together, if not put what is in the String (Free or Unavailable)
         # We use extract_first() so we won't get the item with the [ that comes with it 
-        for steamgames in response.css('#search_result_container div a'): 
-            priceTxt = steamgames.css('div div.search_price::text').extract_first()
+        for steamgames in response.css('#search_result_container div:not([class^="search_pagination"]) a'):
+            priceTxt = steamgames.css('div div div.search_price::text').extract_first()
+            discount = steamgames.css('div div div.search_discount span::text').extract_first()
             # removing the spaces if it's not a number
             priceTxt = re.sub(r"[\s]", "", priceTxt)
+            if(discount is not None and priceTxt == ""):
+            # if (priceTxt == ""):
+                priceTxt = re.sub(r"[\s]", "",steamgames.css("div div.discounted::text")[1].extract())
             if (priceTxt == "FreetoPlay"):
                 priceTxt = "0,0"
-            yield {
-                'name': steamgames.css('div.search_name span.title::text').extract_first(),
-                'price': priceTxt,
-                'link': steamgames.css('::attr(href)').extract_first(),
-                }
+            if(priceTxt != ""):
+                yield {
+                    'nameSteam': steamgames.css('div.search_name span.title::text').extract_first(),
+                    'priceSteam': priceTxt,
+                    'linkSteam': steamgames.css('::attr(href)').extract_first(),
+                    'imgSteam': steamgames.css("div.search_capsule img::attr(src)").extract_first()
+                    }
         # Goes to next item on the list with the links, so the crawler goes to the next page
-        # self.i +=1
-        # next_page = response.css('noscript a::attr(href)')[self.i].extract()
-        # if next_page is not None:
-        #     next_page = next_page
-        #     yield scrapy.Request(next_page, callback=self.parse)
+        self.i +=1
+        # # next_page = response.css('div.search_pagination_right a::attr(href)')[self.i].extract()
+        next_page = "https://store.steampowered.com/search/?category1=998&page="+str(self.i)
+        if next_page is not None:
+            next_page = next_page
+            yield scrapy.Request(next_page, callback=self.parse)
 
 
